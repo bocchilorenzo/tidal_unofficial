55ab810d6fed4561c5f63678952ca023cc4f88ae
Lorenzo Bocchi <lorenzobocchi99@yahoo.com>
2022-08-17 17:12:44 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -18,22 +18,15 @@ author = 'Lorenzo Bocchi'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['recommonmark',
-              'sphinx-press-theme',
-              'sphinx.ext.autodoc',
-              'sphinx.ext.viewcode']
+extensions = [
+    'recommonmark',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'press'
+# -- Options for HTML output ---------------------------
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-html_theme_options = {
-    "external_links": [
-      ("Github", "https://github.com/bocchilorenzo/tidal_unofficial")
-  ]
-}
